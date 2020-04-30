9e9f95aa035eaf7f0a5dba7ae711e14fbc4b8fbf
Fenix <fenix4088@gmail.com>
2020-04-30 13:31:00 +0200

    Colection datas from radio btns

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@
 // 6. Подсветка рамки для радио и чекбоксов
 
 
+// Обьект с сохраненными ответами
+let answers = {
+    2: null,
+    3: null,
+    4: null,
+    5: null
+}
+
+
 
 // Движение вперед
 let btnNext = document.querySelectorAll('[data-nav="next"]');
@@ -13,6 +22,7 @@ btnNext.forEach( function(button) {
     button.addEventListener('click', function() {
 
         let thisCard = this.closest('[data-card]');
+
         // Условие пи котором мы проверяем нужно валидировать эту карточку или нет
         if (thisCard.dataset.validate == "novalidate") {
             console.log("NOVALIDATE");
@@ -22,7 +32,6 @@ btnNext.forEach( function(button) {
             navigate("next", thisCard);
         }
         
-
     });
 });
 
@@ -37,7 +46,7 @@ btnPrev.forEach( function(button) {
     });
 });
 
-
+// Функция для движения вперед и движения назат
 function navigate (direction, thisCard) {
     let thisCardNumber = parseInt(thisCard.dataset.card);
     let nextCard;
@@ -53,5 +62,49 @@ function navigate (direction, thisCard) {
 
     thisCard.classList.add("hidden");
     document.querySelector(`[data-card="${nextCard}"]`).classList.remove('hidden');
+
+}
+
+// Функция которая союирает все данные из текущей карточки
+function gatherCardData (number) {
+    /* 
+        {
+            question: "Ваше любимое блюдо",
+            answer: [
+                {name: 'pirogi', value: "Pirogi"},
+                {name: 'salaty', value: "Salaty"}
+            ]
+        }
+    */
+
+    let question;
+    let result = [];
+
+    // Находим карточку по номеру и по дата атрибуту
+    let curentCard = document.querySelector(`[data-card="${number}"]`);
+   
+    // Находим главный вопрос карточки
+    question = curentCard.querySelector("[data-question]").innerText;
+    
+    // 1. Находим все заполненые значения из радио кнопок
+    let radioValues = curentCard.querySelectorAll('[type="radio"]');
+    // console.log(radioValues);
+    radioValues.forEach( function(item) {
+        if (item.checked) {
+           result.push({
+               name: item.name,
+               value: item.value
+           });
+        }
+    });
+
+    console.log(result);
+
+    let data = {
+        question: question,
+        answer: result
+    }
+
+    return data;
 
 }

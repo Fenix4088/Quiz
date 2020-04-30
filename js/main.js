ede55d776a443204712f54fd33b227b0d28edbee
Fenix <fenix4088@gmail.com>
2020-04-30 11:16:50 +0200

    Creat a function for Next and Prev btn

--- js/main.js
+++ js/main.js
@@ -2,15 +2,9 @@
 let btnNext = document.querySelectorAll('[data-nav="next"]');
 btnNext.forEach( function(button) {
     button.addEventListener('click', function() {
-        console.log("Next");
 
         let thisCard = this.closest('[data-card]');
-        let thisCardNumber = parseInt(thisCard.dataset.card);
-        let nextCard = thisCardNumber + 1;
-
-        thisCard.classList.add("hidden");
-
-        document.querySelector(`[data-card="${nextCard}"]`).classList.remove('hidden');
+        navigate("next", thisCard);
 
     });
 });
@@ -19,15 +13,28 @@ btnNext.forEach( function(button) {
 let btnPrev = document.querySelectorAll('[data-nav="prev"]');
 btnPrev.forEach( function(button) {
     button.addEventListener('click', function() {
-        console.log("Prev");
 
         let thisCard = this.closest('[data-card]');
-        let thisCardNumber = parseInt(thisCard.dataset.card);
-        let prevCard = thisCardNumber - 1;
-
-        thisCard.classList.add("hidden");
-
-        document.querySelector(`[data-card="${prevCard}"]`).classList.remove('hidden');
+        navigate("prev", thisCard);
 
     });
 });
+
+
+function navigate (direction, thisCard) {
+    let thisCardNumber = parseInt(thisCard.dataset.card);
+    let nextCard;
+    if (direction == "next") {
+
+        nextCard = thisCardNumber + 1;
+
+    } else if (direction == "prev") {
+
+        nextCard = thisCardNumber - 1;
+
+    }
+
+    thisCard.classList.add("hidden");
+    document.querySelector(`[data-card="${nextCard}"]`).classList.remove('hidden');
+
+}

fc378522b5a5ef4a5df1b30e75ca3e6c7fbcc91d
Fenix <fenix4088@gmail.com>
2020-04-30 11:34:09 +0200

    Validation prepared

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
+// 1. Функционал перемещения по карточкам
+// 2. Проверка на ввод данных
+// 3. Сбор данных с карточек
+// 4. Записывать все введенные данные
+// 5. Рализовать работу прогресс бара
+// 6. Подсветка рамки для радио и чекбоксов
+
+
+
 // Движение вперед
 let btnNext = document.querySelectorAll('[data-nav="next"]');
 btnNext.forEach( function(button) {
     button.addEventListener('click', function() {
 
         let thisCard = this.closest('[data-card]');
-        navigate("next", thisCard);
+        // Условие пи котором мы проверяем нужно валидировать эту карточку или нет
+        if (thisCard.dataset.validate == "novalidate") {
+            console.log("NOVALIDATE");
+            navigate("next", thisCard);
+        } else {
+            console.log("VALIDATE");
+            navigate("next", thisCard);
+        }
+        
 
     });
 });
